786a5ac0c443801f71ac427b12b39fd358a10f64
Savita Seetaraman <savita.seetaraman5@gmail.com>
2021-03-16 21:34:31 +0100

    Updated trainings page - moved up upcoming training

--- components/schedule/Trainings/index.jsx
+++ components/schedule/Trainings/index.jsx
@@ -38,130 +38,6 @@ const ShaktiTrainings = ({ hideTitle = false }) => {
 
     const TrainersList = [
         {
-            "slug": "symbolic-execution-angr-for-ctf",
-            "title": "Symbolic execution / angr for CTF",
-            "sessions": [
-                {
-                    "date": "Friday, 12th March",
-                    "time": "20:30 - 22:30 IST",
-                }
-            ],
-            "abstract": <>
-                <p>
-                    Symbolic execution is a powerful tool for code verification, bug hunting and reverse engineering.
-                    In this class, we will dive into the concepts of constraint programming and SMT solvers and how
-                    binary analysis tools, such as angr, integrate these concepts into their frameworks.
-                </p>
-                <p>
-                    It is going to be a very practical class, where we are going to solve various CTF challenges with
-                    the goal of visiting different features of angr.
-                </p>
-                <p>
-                    Most of CTF players use z3 and angr to save time when solving reverse engineering challenge and
-                    that is also the path we are going to take.
-                </p>
-                <p>
-                    If time is available, we will also check manticore and miasm, two other tools with symbolic
-                    execution engines with different features!
-                </p>
-            </>,
-            "prerequisite": [
-                "Be comfortable understanding and writing Python3 code",
-                "Familiarity with x86/x64 assembly"
-            ],
-            "requirements": [
-                "VMware Workstation or Player (at least version 12) (no VirtualBox)",
-                "At least 8GB of RAM", "At least 40GB of free disk space",
-                "A laptop with administrative privileges"
-            ],
-            "trainer": {
-                "name": "Thaís Moreira Hamasaki",
-                "designation": "Offensive Security Researcher",
-                "company": "Intel",
-                "image": require('../../../images/speakers/thais_moreira_hamasaki.jpeg'),
-                "links": {
-                    "twitter": "https://twitter.com/barbieauglend",
-                    "linkedin": "https://www.linkedin.com/in/barbieauglend/",
-                    "website": "https://barbieauglend.re/"
-                },
-                "bio": <>
-                    <p>
-                        Thaís Moreira Hamasaki is an Offensive Security Researcher @ Intel - STORM. Previous to that, she
-                        worked as a Malware Researcher @ F-Secure, with a focus on static analysis, reverse engineering,
-                        and logical programming.
-                    </p>
-                    <p>
-                        Thaís started her career within the anti-virus industry working on data and malware analysis,
-                        where she developed her knowledge of threat protection systems. She won the "best rookie speaker"
-                        award from BSides London for her very first talk about "Using SMT solvers to deobfuscate malware binaries".
-                        Recent research topics include platform security, SMM, and GFX. She is a member of the
-                        Düsseldorf Hackerspace aka Chaosdorf, where she also leads the groups for Reverse Engineering and
-                        x86 Assembly. In her free time, you can find Thaís writing bad code, cooking, or climbing
-                        somewhere offline.
-                    </p>
-                </>,
-            },
-            "registerURL": "https://register.shakticon.com/"
-        },
-        {
-            "slug": "understanding-tls-mitm-attacks",
-            "title": "Understanding TLS and MITM Attacks",
-            "sessions": [
-                {
-                    "date": "Saturday, 13th March",
-                    "time": "12:30 - 15:30 IST"
-                },
-                {
-                    "date": "Sunday, 14th March",
-                    "time": "12:30 - 15:30 IST"
-                }
-            ],
-            "registerURL": "https://register.shakticon.com/",
-            "abstract": <>
-                <p>Have you ever wanted to know why Firefox warns you about concretely when the big "THIS PAGE IS UNSECURE" appears ?</p>
-                <p>Well, in this workshop we will be doing what Firefox tries to protect you from: Eavesdropping someone's network.</p>
-                <div>We'll go from theory to practice:</div>
-                <div>How do you get packets supposed to go from a computer to another to come to your machine ?</div>
-                <div>
-                    That's called the Women-In-The-Middle attack, and we'll use Scapy (pre-installed in kali linux) to
-                    do that using a technique called ARP-Spoofiing.
-                </div>
-                <div>
-                    And even if this look fun, you might know that nowadays traffic is encrypted so what can we do with these encrypted packets ?
-                </div>
-                <div>
-                    Firstly we'll go through the basics of TLS, seeing how does your connection gets encrypted and how the trust
-                    between client and server is built : that's the part where certificates signature and verification arrives.
-                </div>
-                <div>
-                    With these basics, we'll see what could go wrong, what happens if you say to firefox "No, I understand the risks and continue" from the attacker's perspective.
-                </div>
-                <div>
-                    I have prepared pair of VMs for each of you and you will be able to really eavesdrop on this small environment - it's really a hands on intro to WITM attacks and network interception.
-                </div>
-                <div>
-                    After this training, you will:
-                </div>
-                <ul>
-                    <li>have performed a WITM attack with Scapy</li>
-                    <li>know how to manually generate TLS certificates</li>
-                    <li>have a better idea of what Burp or ZAP proxy do when they "intercept TLS traffic"</li>
-                    <li>better understand your browser's security messages</li>
-                    <li>have an overview of what TLS is for and what is concretly protects us from</li>
-                </ul>
-            </>,
-            "trainer": {
-                "name": "Caroline Leman",
-                "designation": "Security Engineer",
-                "company": "ANSSI",
-                "image": require('../../../images/speakers/caroline.JPG'),
-                "bio": <p>
-                    Caroline is a reverse engineer, security enthousiast and feminist. She has been contributing
-                    to miasm (https://github.com/cea-sec/miasm).
-                </p>
-            },
-        },
-        {
             "slug": "reversing-cpp-binaries",
             "title": "Reverse Engineering C++ binaries",
             "sessions": [
@@ -368,6 +244,130 @@ const ShaktiTrainings = ({ hideTitle = false }) => {
                 </p>
             },
             // "registerURL": "https://docs.google.com/forms/d/e/1FAIpQLSe_wWy9Q8E_CExv_1V5xOiDY3RnjH9FCvG28ljaaWj8FQZEWA/viewform?usp=send_form"
+        },
+        {
+            "slug": "symbolic-execution-angr-for-ctf",
+            "title": "Symbolic execution / angr for CTF",
+            "sessions": [
+                {
+                    "date": "Friday, 12th March",
+                    "time": "20:30 - 22:30 IST",
+                }
+            ],
+            "abstract": <>
+                <p>
+                    Symbolic execution is a powerful tool for code verification, bug hunting and reverse engineering.
+                    In this class, we will dive into the concepts of constraint programming and SMT solvers and how
+                    binary analysis tools, such as angr, integrate these concepts into their frameworks.
+                </p>
+                <p>
+                    It is going to be a very practical class, where we are going to solve various CTF challenges with
+                    the goal of visiting different features of angr.
+                </p>
+                <p>
+                    Most of CTF players use z3 and angr to save time when solving reverse engineering challenge and
+                    that is also the path we are going to take.
+                </p>
+                <p>
+                    If time is available, we will also check manticore and miasm, two other tools with symbolic
+                    execution engines with different features!
+                </p>
+            </>,
+            "prerequisite": [
+                "Be comfortable understanding and writing Python3 code",
+                "Familiarity with x86/x64 assembly"
+            ],
+            "requirements": [
+                "VMware Workstation or Player (at least version 12) (no VirtualBox)",
+                "At least 8GB of RAM", "At least 40GB of free disk space",
+                "A laptop with administrative privileges"
+            ],
+            "trainer": {
+                "name": "Thaís Moreira Hamasaki",
+                "designation": "Offensive Security Researcher",
+                "company": "Intel",
+                "image": require('../../../images/speakers/thais_moreira_hamasaki.jpeg'),
+                "links": {
+                    "twitter": "https://twitter.com/barbieauglend",
+                    "linkedin": "https://www.linkedin.com/in/barbieauglend/",
+                    "website": "https://barbieauglend.re/"
+                },
+                "bio": <>
+                    <p>
+                        Thaís Moreira Hamasaki is an Offensive Security Researcher @ Intel - STORM. Previous to that, she
+                        worked as a Malware Researcher @ F-Secure, with a focus on static analysis, reverse engineering,
+                        and logical programming.
+                    </p>
+                    <p>
+                        Thaís started her career within the anti-virus industry working on data and malware analysis,
+                        where she developed her knowledge of threat protection systems. She won the "best rookie speaker"
+                        award from BSides London for her very first talk about "Using SMT solvers to deobfuscate malware binaries".
+                        Recent research topics include platform security, SMM, and GFX. She is a member of the
+                        Düsseldorf Hackerspace aka Chaosdorf, where she also leads the groups for Reverse Engineering and
+                        x86 Assembly. In her free time, you can find Thaís writing bad code, cooking, or climbing
+                        somewhere offline.
+                    </p>
+                </>,
+            },
+            "registerURL": "https://register.shakticon.com/"
+        },
+        {
+            "slug": "understanding-tls-mitm-attacks",
+            "title": "Understanding TLS and MITM Attacks",
+            "sessions": [
+                {
+                    "date": "Saturday, 13th March",
+                    "time": "12:30 - 15:30 IST"
+                },
+                {
+                    "date": "Sunday, 14th March",
+                    "time": "12:30 - 15:30 IST"
+                }
+            ],
+            "registerURL": "https://register.shakticon.com/",
+            "abstract": <>
+                <p>Have you ever wanted to know why Firefox warns you about concretely when the big "THIS PAGE IS UNSECURE" appears ?</p>
+                <p>Well, in this workshop we will be doing what Firefox tries to protect you from: Eavesdropping someone's network.</p>
+                <div>We'll go from theory to practice:</div>
+                <div>How do you get packets supposed to go from a computer to another to come to your machine ?</div>
+                <div>
+                    That's called the Women-In-The-Middle attack, and we'll use Scapy (pre-installed in kali linux) to
+                    do that using a technique called ARP-Spoofiing.
+                </div>
+                <div>
+                    And even if this look fun, you might know that nowadays traffic is encrypted so what can we do with these encrypted packets ?
+                </div>
+                <div>
+                    Firstly we'll go through the basics of TLS, seeing how does your connection gets encrypted and how the trust
+                    between client and server is built : that's the part where certificates signature and verification arrives.
+                </div>
+                <div>
+                    With these basics, we'll see what could go wrong, what happens if you say to firefox "No, I understand the risks and continue" from the attacker's perspective.
+                </div>
+                <div>
+                    I have prepared pair of VMs for each of you and you will be able to really eavesdrop on this small environment - it's really a hands on intro to WITM attacks and network interception.
+                </div>
+                <div>
+                    After this training, you will:
+                </div>
+                <ul>
+                    <li>have performed a WITM attack with Scapy</li>
+                    <li>know how to manually generate TLS certificates</li>
+                    <li>have a better idea of what Burp or ZAP proxy do when they "intercept TLS traffic"</li>
+                    <li>better understand your browser's security messages</li>
+                    <li>have an overview of what TLS is for and what is concretly protects us from</li>
+                </ul>
+            </>,
+            "trainer": {
+                "name": "Caroline Leman",
+                "designation": "Security Engineer",
+                "company": "ANSSI",
+                "image": require('../../../images/speakers/caroline.JPG'),
+                "bio": <p>
+                    Caroline is a reverse engineer, security enthousiast and feminist. She has been contributing
+                    to miasm (https://github.com/cea-sec/miasm).
+                </p>
+            },
         },
     ]
 
