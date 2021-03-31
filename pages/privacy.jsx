3153e77325300180b39c18c655f5f1e514d9ef3d
Ashwin S Shenoy <aswinshenoy65@gmail.com>
2021-03-31 18:00:40 +0530

    Add privacy policy page

---
+++ pages/privacy.jsx
@@ -0,0 +1,212 @@
+import Base from "../components/shared";
+import Topbar from "../components/shared/Topbar";
+import FooterArea from "../components/landing/Footer";
+
+
+const PrivacyPage = () => {
+    return <Base meta={{title: 'Privacy Policy'}}>
+        <Topbar/>
+        <div className="container-lg p-2">
+            <h1 className="my-5 display-4">Privacy Policy</h1>
+            <div>Last Updated on March 15, 2020</div>
+
+            <p>
+                This Privacy Policy details the manner in which ShaktiCon, Amrita Vishwa Vidyapeetham
+                (“ShaktiCon/Us/Our/We”) collects, restores, deals and handles the end users’ (“You/Your/User”)
+                information made available by the User as a result of such User’s access to and use the services
+                provided by ShaktiCon through ShaktiCon WebApp (which term shall be deemed to include the internet and
+                mobile website www.shakticon.com).
+            </p>
+
+            <p>
+                By mere use of the ShaktiCon WebApp (“WebApp”), You expressly consent to Our use and disclosure of Your
+                personal information in accordance with this Privacy Policy. This Privacy Policy is incorporated into
+                and subject to the T&C.
+            </p>
+
+            <h2>Information We Collect</h2>
+            <p>
+                When You use the WebApp, We collect and store Your personal information which is provided by You from
+                time to time. Our primary goal in doing so is to provide you a safe, efficient, smooth and customized
+                experience. This allows Us to provide services and features that most likely meet Your needs, and to
+                customize WebApp to make Your experience safer and easier. More importantly, while doing so We may
+                collect
+                personal information from You that We consider necessary for achieving this purpose.
+            </p>
+
+            <p>
+                In general, You can browse the WebApp without telling Us who You are or revealing any personal
+                information about Yourself. Once You give Us Your personal information, You are not anonymous to us.
+                Where possible,
+                We indicate which fields are required and which fields are optional. You always have the option to not
+                provide information by choosing not to use a particular service or feature on the WebApp. We may
+                automatically
+                track certain information about You based upon Your behaviour on Our WebApp. We use this information to
+                do
+                internal research on Our users' demographics, interests, and behaviour to better understand, protect and
+                serve Our users. This information is compiled and analysed on an aggregated basis. This information may
+                include the URL that you just came from (whether this URL is on Our WebApp or not), which URL You next
+                go to (whether this URL is on Our WebApp or not), Your computer browser information, and Your IP
+                address.</p>
+
+            <p>We use data collection devices such as "cookies" on certain pages of Our WebApp to help analyse Our web
+                page flow, measure promotional effectiveness, and promote trust and safety. "Cookies" are small files
+                placed on Your hard drive that assist Us in providing Our services. We offer certain features that are
+                only available through the use of a "cookie".</p>
+
+            <p>We also use cookies to allow You to enter Your password less frequently during a session. Cookies can
+                also
+                help Us provide information that is targeted to Your interests. Most cookies are "session cookies,"
+                meaning
+                that they are automatically deleted from Your hard drive at the end of a session. You are always free to
+                decline Our cookies if Your browser permits, although in that case You may not be able to use certain
+                features on the WebApp and You may be required to re-enter Your password more frequently during a
+                session.</p>
+
+            <p>Additionally, You may encounter "cookies" or other similar devices on certain pages of the WebApp that
+                are
+                placed by third parties. We do not control the use of cookies by third parties.</p>
+
+            <p>If You send Us personal correspondence, such as emails or letters, or if other users or third parties
+                send
+                Us correspondence about Your activities or postings on our WebApp, We may collect such information into
+                a
+                file specific to you.</p>
+
+            <p>We collect personally identifiable information (email address, name, mobile number, college details,
+                college
+                ID, photo, location etc.) from you when You set up an account with us. While You can browse some
+                sections of
+                Our WebApp without being a registered member, certain activities (such as registering for an event) do
+                require registration. We do use Your contact information to send You offers based on Your previous
+                orders
+                and Your interests.</p>
+
+            <h2>Use of Demographic / Profile Data / Your Information</h2>
+            <p>We use personal information to provide the services You request. To the extent possible, We use Your
+                personal information to market to You or share information with certain other parties to market to You,
+                We
+                will provide You the ability to opt-out of such uses. We use Your personal information to resolve
+                disputes;
+                troubleshoot problems; help promote a safe service; collect money; measure consumer interest in Our
+                products
+                and services, inform You about online and offline offers, products, services, and updates; customize
+                Your
+                experience; detect and protect Us against error, fraud and other criminal activity; enforce our terms
+                and
+                conditions; and as otherwise described to You at the time of collection.</p>
+
+            <p>In Our efforts to continually improve Our product and service offerings, We collect and analyse
+                demographic
+                and profile data about Our users' activity on our WebApp.</p>
+
+            <p>We identify and use Your IP address to help diagnose problems with Our server, and to administer the
+                platform. Your IP address is also used to help identify you and to gather broad demographic
+                information.</p>
+
+            <p>We use Your personal information to prevent identity-theft, and to protect Us against error, fraud and
+                other
+                criminal activity. Your personal information shall be accessible by Our volunteer team only during the
+                event, if a case arises that they need to identify you for providing you services, or to ensure
+                security.</p>
+
+            <h3>Cookies</h3>
+
+            <p>A "cookie" is a small piece of information stored by a web server on a web browser so it can be later
+                read
+                back from that browser. Cookies are useful for enabling the browser to remember information specific to
+                a
+                given user. We place both permanent and temporary cookies in Your computer's hard drive. The cookies do
+                not
+                contain any of Your personally identifiable information.</p>
+
+            <h2>Sharing of Personal Information</h2>
+            <p>We may share personal information You share with Us, with our affiliates, and our partner (as defined
+                under
+                the T&C, available here) affiliates for various purposes, including but not limited to help with the
+                detection and prevention of identity theft, fraud and other potentially illegal acts; correlate related
+                or
+                multiple accounts to prevent abuse of Our services; to facilitate marketing and promotional
+                activities.</p>
+
+            <p>We may also disclose personal information if required to do so by law or in the good faith belief that
+                such
+                disclosure is reasonably necessary to respond to subpoenas, court orders, or other legal process. We may
+                disclose personal information to law enforcement offices, third party rights owners, or others in the
+                good
+                faith belief that such disclosure is reasonably necessary to: enforce Our Terms and Conditions (“T&C”)
+                or
+                Privacy Policy; respond to claims that an advertisement, posting or other content violates the rights of
+                a
+                third party; or protect the rights, property or personal safety of Our users or the general public.</p>
+
+            <h2>Advertisements on WebApp</h2>
+            <p>We do not use any third-party advertising companies to serve ads when You visit our WebApp.</p>
+
+            <h2>Links to Other Sites</h2>
+            <p>Our WebApp may show up links to other websites/apps, including but not limited to our sponsors and
+                partners,
+                which may collect personally identifiable information about You. We are not responsible for the privacy
+                practices or the content of those linked websites. Please refer to the privacy policies of those for
+                clarification regarding the same.</p>
+
+            <h2>Choice/Opt-Out</h2>
+            <p>We provide all Users with the opportunity to opt-out of receiving non-essential (promotional,
+                marketing-related) communications from Us on behalf of our partners, and from Us in general, after
+                setting
+                up an account.</p>
+
+            <p>If You want to remove Your contact information from all Our WebApp lists and newsletters, please click on
+                the unsubscribe links in the emailer. If that is however, not available then please contact us using the
+                details below.</p>
+
+            <h2>Children’s Information</h2>
+            <p>Another part of our priority is adding protection for children while using the internet. We encourage
+                parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
+
+            <p>We do not knowingly collect any Personal Identifiable Information from children under the age of 13. If
+                you
+                think that your child provided this kind of information on Our WebApp, we strongly encourage you to
+                contact
+                us immediately and we will do our best efforts to promptly remove such information from our records.</p>
+
+            <h2>Security Precautions</h2>
+            <p>Our WebApp has stringent security measures in place to protect the loss, misuse, and alteration of the
+                information under Our control. Whenever You change or access Your account information, We offer the use
+                of a
+                secure server. Once your information is in Our possession We adhere to strict security guidelines,
+                protecting it against unauthorized access.</p>
+
+            <h2>Deletion of Data</h2>
+            <p>
+                We shall permanently delete certain information You shared with Us including but not limited to your
+                personal information, automatically within a week after the event is completed. We may keep some
+                information You shared with us such as, but not limited to your name, your email address,
+                your college details, feedback and surveys etc. even after the event.
+            </p>
+
+            <p>
+                At any point of time, You have the option to ask for deletion of the information You share with Us,
+                either through the edit profile section in Our WebApp or by contacting us through the details given below.
+                However, we might continue to retain certain details such as, but not limited to Your name,
+                email, phone number in case this request is made after you making purchase of our service,
+                or participating in the event, as governed by Our T&C.
+            </p>
+
+            <h2>Your Consent</h2>
+            <p>
+                By using the Our WebApp and/or by providing Your information, You consent to the collection and use of
+                the information You disclose on Our WebApp in accordance with this Privacy Policy, including but
+                not limited to Your consent for sharing your information as per this Privacy Policy.
+            </p>
+
+            <p>
+                If We decide to change Our Privacy Policy, We will post those changes on this page so that You are always
+                aware of what information We collect, how We use it, and under what circumstances We disclose it.
+            </p>
+        </div>
+        <FooterArea/>
+    </Base>;
+};
+
+export default PrivacyPage;
